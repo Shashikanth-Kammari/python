0084c1bc0087fd6e117f09f670f50b43cbc5ab0c
Shashi <shashikanth0312@gmail.com>
2026-07-12 23:19:33 +0530

    adding

---
+++ condition.py
@@ -0,0 +1,8 @@
+user_input= int(input("Enter the number : "))
+
+#user_input= int(user_input)
+
+if user_input >= 10:
+    print("your enter the greater or equal than 10")
+else:
+    print("your entering the less than 10  ")
