426b728017ffd460cd44d908d221478a10761709
Shashi <shashikanth0312@gmail.com>
2026-07-12 13:10:46 +0530

    adding

--- slicing.py
+++ slicing.py
@@ -1,5 +1,19 @@
 name="shashikanth"
-
 indexing=name[6:11]
-
 print(indexing) 
+
+
+#if we want to odd number letters
+
+oddslice=name[3:11:2]
+
+print(oddslice)
+
+reverse=name[::-1]
+
+print(reverse)
+
+#lenth of a string
+
+length=len(name)
+print(length)
