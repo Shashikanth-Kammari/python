09ba5ed923e290c56bd20708e1faecf18966f7f7
Shashi <shashikanth0312@gmail.com>
2026-07-12 12:39:58 +0530

    adding

---
+++ demo.py
@@ -0,0 +1,12 @@
+number=121
+temp=number
+reverse=0
+
+while temp>0:
+    digit=temp%10    #it remaider store that is 1
+    reverse=reverse*10+digit   # it first reversed number
+    temp=temp//10  #it give removed last digit that is 1 now it is 12
+if number == reverse:
+    print("this number is Palidrome number "+ str(reverse))
+else:
+    print("this number is not palidrome number "+ str(reverse))
