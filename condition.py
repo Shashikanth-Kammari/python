e21d509f88455d405bb0cc2682df6d030614cac3
Shashi <shashikanth0312@gmail.com>
2026-07-12 23:41:20 +0530

    adding

--- condition.py
+++ condition.py
@@ -6,3 +6,27 @@ if user_input >= 10:
     print("your enter the greater or equal than 10")
 else:
     print("your entering the less than 10  ")
+
+#Method2
+
+user_input= int(input("Enter the number : "))
+if user_input == 10:
+    print("user input is equals to 10")
+elif user_input > 10:
+    print("user input is greater than 10")
+elif user_input < 10:
+    print("user input less than 10")
+else:
+    print("please enter valid value")
+
+#exception  handling 
+try:
+    user_input= int(input("Enter the number : "))
+    if user_input == 10:
+        print("user input is equals to 10")
+    elif user_input > 10:
+        print("user input is greater than 10")
+    elif user_input < 10:
+        print("user input less than 10")
+except:
+    print("please enter valid value")
