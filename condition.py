8a418cdc9be840dd87362449f23dd1b39f7d0260
Shashi <shashikanth0312@gmail.com>
2026-07-14 20:56:34 +0530

    adding

--- condition.py
+++ condition.py
@@ -1,32 +1,32 @@
-user_input= int(input("Enter the number : "))
+userinputs= int(input("Enter the number : "))
 
-#user_input= int(user_input)
+#userinputs= int(userinputs)
 
-if user_input >= 10:
+if userinputs >= 10:
     print("your enter the greater or equal than 10")
 else:
     print("your entering the less than 10  ")
 
 #Method2
 
-user_input= int(input("Enter the number : "))
-if user_input == 10:
-    print("user_input input is equals to 10")
-elif user_input > 10:
-    print("user_input input is greater than 10")
-elif user_input < 10:
-    print("user_input input less than 10")
+userinputs= int(input("Enter the number : "))
+if userinputs == 10:
+    print("userinputs input is equals to 10")
+elif userinputs > 10:
+    print("userinputs input is greater than 10")
+elif userinputs < 10:
+    print("userinputs input less than 10")
 else:
     print("please enter valid value")
 
 #exception  handling 
 try:
-    user_input= int(input("Enter the number : "))
-    if user_input == 10:
-        print("user_input input is equals to 10")
-    elif user_input > 10:
-        print("user_input input is greater than 10")
-    elif user_input < 10:
-        print("user_input input less than 10")
+    userinputs= int(input("Enter the number : "))
+    if userinputs == 10:
+        print("userinputs input is equals to 10")
+    elif userinputs > 10:
+        print("userinputs input is greater than 10")
+    elif userinputs < 10:
+        print("userinputs input less than 10")
 except:
     print("please enter value")
